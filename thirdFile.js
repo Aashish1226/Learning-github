4dda5ed56d599ea6db4e74d892cf8de146306007
Aashish Saini <intern@modulobytes.com>
2025-01-17 16:50:56 +0530

    latest

--- thirdFile.js
+++ thirdFile.js
@@ -4,3 +4,7 @@ function newFun(){
     console.log("HI , i am another new line in the function in the thirdFile.js");
     
 }
+function anotherFun(){
+    console.log("Hi , i am the anotherFun function in the thirdFile.js ");
+    
+}
