31e9f306b91094b309ab80bb261f5dfa28ab3324
Aashish Saini <intern@modulobytes.com>
2025-01-17 14:41:25 +0530

    I am the second commit in the first-branch

--- thirdFile.js
+++ thirdFile.js
@@ -1,5 +1,6 @@
 console.log("Hi , i am thirdFile");
 function newFun(){
     console.log("Hi , i am new Function in the thirdFile.js");
+    console.log("HI , i am another new line in the function in the thirdFile.js");
     
 }
